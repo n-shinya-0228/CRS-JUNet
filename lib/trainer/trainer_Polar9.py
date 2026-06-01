57c39d509b35135a2c458e2183094d066fd92a54
n-shinya-0228 <shinya.n.0228@gmail.com>
2026-06-01 19:45:08 +0900

    step1の学習開始

--- lib/trainer/trainer_Polar9.py
+++ lib/trainer/trainer_Polar9.py
@@ -4,7 +4,7 @@
 # - combines CE/Focal + Lovasz (optional) + Boundary BCE + Aux losses
 # - keeps scheduler/optimizer structure + EMA model for better mIoU
 
-#trainer_Polar8
+#trainer_Polar9
 import torch
 torch.set_float32_matmul_precision('high')
 import torch._dynamo
